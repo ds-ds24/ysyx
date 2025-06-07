e016a35afb54aab07294782b9e4677f513253b6d
tracer-ysyx <tracer@ysyx.org>
2025-06-07 11:45:21 +0000

    > sim RTL ysyx_22040000 窦硕 Linux ds24 6.8.0-60-generic #63-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 18:51:58 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  11:45:21 up 10 days, 17:35,  1 user,  load average: 0.18, 0.06, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,37 @@
-#include <stdio.h>
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+static Vtop* top;
+
+void sim_init(){
+	contextp = new VerilatedContext;
+	tfp = new VerilatedVcdC;
+	top = new Vtop;
+	contextp->traceEverOn(true);
+	top->trace(tfp,0);
+	tfp->open("wave.vcd");
+}
+
+void step_and_dump_wave(){
+	top->eval();
+	contextp->timeInc(1);
+	tfp->dump(contextp->time());
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
+  sim_init();
+  while(contextp->time()<100){
+        int a=rand()&1;
+        int b=rand()&1;
+        top->a=a;
+        top->b=b;
+        step_and_dump_wave();
+        assert(top->f == (a ^ b));
+  }
+  tfp->close();
+
   return 0;
 }
