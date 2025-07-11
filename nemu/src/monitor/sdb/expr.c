26e705ad92982d760f56986dcd93ff9156fb4af8
tracer-ysyx <tracer@ysyx.org>
2025-07-11 14:08:10 +0000

    > compile NEMU ysyx_22040000 窦硕 Linux ds24 6.8.0-60-generic #63-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 18:51:58 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  14:08:10 up 20 days, 23:20,  3 users,  load average: 0.09, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -164,7 +164,7 @@ static bool check_parentheses(int p,int q){
 
 int eval(int p,int q) {
     if(p>q){
-      printf("emoo\n");
+      printf("其实位置大于末位\n");
       return 0;
     }
     else if(p==q){
