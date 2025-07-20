442fd30fd9a7534a7c722a6e40b692ccc05b54dd
tracer-ysyx <tracer@ysyx.org>
2025-07-20 14:02:45 +0000

    > sim RTL ysyx_22040000 窦硕 Linux ds24 6.8.0-60-generic #63-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 18:51:58 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  14:02:45 up 24 days, 14:13,  3 users,  load average: 0.23, 0.16, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,7 +3,7 @@
 #include <nvboard.h>
 
 static TOP_NAME dut;
-void nvboard_bind_all_pins(TOP_NAME* top) ;
+void nvboard_bind_all_pins(TOP_NAME* top);
 //static void single_cycle() {
   //dut.clk = 0; dut.eval();
   //dut.clk = 1; dut.eval();}
